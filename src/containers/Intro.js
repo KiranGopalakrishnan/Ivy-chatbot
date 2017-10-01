2176b166a78e8ce95b7c844e812d39fa7621df7e
KiranGopalakrishnan <kirangplkrishnan@gmail.com>
2017-10-01 13:19:40 -0400

    Animated text fixes

--- src/containers/Intro.js
+++ src/containers/Intro.js
@@ -5,16 +5,19 @@ class Intro extends Component{
   constructor(){
     super();
     this.state={
-      show:false
+      show:true
     };
   }
   componentDidMount=()=>{
-    this.setState({show:true});
+    setInterval(()=>{
+      var st = this.state.show?false:true;
+      this.setState({show:st});
+    },5000)
   }
   render(){
     return(
       <div className='introWrapper'>
-        <h1 className={this.state.show?'animate':'noAnimate'}>Welcome to IVY</h1>
+        <h1 className={this.state.show?'animatedText animate':'animatedText exit'}>Hello...</h1>
       </div>
     );
   }
